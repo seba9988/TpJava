2d9c9d96f039e4d092587b26641c376196a97383
Sebastian Ricalde <seba9988@hotmail.com>
2022-05-20 23:20:26 -0300

    reforma de DataEquipo con try with parameter y revision de querys

--- src/data/DataPartido.java
+++ src/data/DataPartido.java
@@ -13,49 +13,45 @@ import entities.*;
 public class DataPartido {
 	
 	public LinkedList<Partido> getAll () {
-		Statement stm = null;
-		ResultSet rs = null;
+		String getAllStatement="SELECT p.fecha,p.hora,p.resultado,p.incidencias, eq1.id,eq1.razonSocial,eq1.localidad,eq1.puntaje,eq1.difGoles, eq2.id,eq2.razonSocial,eq2.localidad,eq2.puntaje,eq2.difGoles, c.numCancha,c.nombre, arb.dniArbitro, arb.nombre, arb.apellido, arb.fechaNac FROM partido p inner join equipo eq1 on p.idEquipo1=eq1.id inner join equipo eq2 on p.idEquipo2=eq2.id inner join cancha c on p.numCancha=c.numCancha left join arbitro arb on arb.dniArbitro=p.dniArbitro";
 		LinkedList<Partido> Partidos = new LinkedList<>();
-		try {
-			stm = DbConnector.getInstancia().getConn().createStatement();
-			rs = stm.executeQuery("SELECT p.fecha,p.hora,p.resultado,p.incidencias,"
-					+ "eq1.id,eq1.razonSocial,eq1.localidad,eq1.puntaje,eq1.difGoles,"
-					+ "eq2.id,eq2.razonSocial,eq2.localidad,eq2.puntaje,eq2.difGoles,"
-					+ "c.numCancha,c.nombre"
-					+ "FROM partido p inner join equipo eq1 on p.idEquipo1=eq1.id "
-					+ "inner join equipo eq2 on p.idEquipo2=eq2.id "
-					+ "inner join cancha c on p.numCancha=c.numCancha;"); // hacer join con equipo , cancha y arbitro
-			while (rs.next()) {
-								Partido partido = new Partido();
-								Equipo equipo1=new Equipo();
-								Equipo equipo2=new Equipo();
-								Cancha cancha=new Cancha();
-								partido.setFecha(rs.getObject("fecha", LocalDate.class));
-								partido.setHora(rs.getObject("hora", LocalTime.class));
-								partido.setResultado(rs.getString("resultado"));
-								partido.setIncidencias(rs.getString("incidencias"));
-								equipo1.setIdEquipo(rs.getInt("eq1.id"));
-								equipo1.setNombre(rs.getString("eq1.razonSocial"));
-								equipo1.setLocalidad(rs.getString("eq1.localidad"));
-								equipo1.setPuntaje(rs.getInt("eq1.puntaje"));
-								equipo1.setDifGoles(rs.getInt("eq1.difGoles"));
-								equipo2.setIdEquipo(rs.getInt("eq2.id"));
-								equipo2.setNombre(rs.getString("eq2.razonSocial"));
-								equipo2.setLocalidad(rs.getString("eq2.localidad"));
-								equipo2.setPuntaje(rs.getInt("eq2.puntaje"));
-								equipo2.setDifGoles(rs.getInt("eq2.difGoles"));
-								cancha.setNroCancha(rs.getInt("c.numCancha"));
-								cancha.setNombre(rs.getString("c.nombre"));
-								partido.setEquipo1(equipo1);
-								partido.setEquipo2(equipo2);
-								partido.setCancha(cancha);
-								Partidos.add(partido);
-								}
-			}catch(Exception ex) {ex.printStackTrace();}
+		try(Statement stm = DbConnector.getInstancia().getConn().createStatement();
+			ResultSet rs =stm.executeQuery(getAllStatement)) {
+			while (rs.next()) {			
+				Partido partido = new Partido();
+				Equipo equipo1=new Equipo();
+				Equipo equipo2=new Equipo();
+				Cancha cancha=new Cancha();
+				Arbitro arbitro= new Arbitro();
+				partido.setFecha(rs.getObject("fecha", LocalDate.class));
+				partido.setHora(rs.getObject("hora", LocalTime.class));
+				partido.setResultado(rs.getString("resultado"));
+				partido.setIncidencias(rs.getString("incidencias"));
+				equipo1.setIdEquipo(rs.getInt("eq1.id"));
+				equipo1.setNombre(rs.getString("eq1.razonSocial"));
+				equipo1.setLocalidad(rs.getString("eq1.localidad"));
+				equipo1.setPuntaje(rs.getInt("eq1.puntaje"));
+				equipo1.setDifGoles(rs.getInt("eq1.difGoles"));
+				equipo2.setIdEquipo(rs.getInt("eq2.id"));
+				equipo2.setNombre(rs.getString("eq2.razonSocial"));
+				equipo2.setLocalidad(rs.getString("eq2.localidad"));
+				equipo2.setPuntaje(rs.getInt("eq2.puntaje"));
+				equipo2.setDifGoles(rs.getInt("eq2.difGoles"));
+				cancha.setNroCancha(rs.getInt("c.numCancha"));
+				cancha.setNombre(rs.getString("c.nombre"));
+				arbitro.setDni(rs.getString("arb.dniArbitro"));
+				arbitro.setNombre(rs.getString("arb.nombre"));
+				arbitro.setApellido(rs.getString("arb.apellido"));
+				arbitro.setFecha_nacimiento(rs.getObject("arb.fechaNac",LocalDate.class));
+				partido.setEquipo1(equipo1);
+				partido.setEquipo2(equipo2);
+				partido.setCancha(cancha);
+				partido.setArbitro(arbitro);
+				Partidos.add(partido);							}
+			}
+		catch(Exception ex) {ex.printStackTrace();}
 		finally {
-			try {
-				if (rs!= null) {rs.close();}
-				if (stm != null) {stm.close();}			
+			try {	
 				DbConnector.getInstancia().releaseConn();
 			} catch (Exception e2) {
 				e2.printStackTrace();
@@ -64,51 +60,49 @@ public class DataPartido {
 		return Partidos;
 	}
 	public Partido getOne(Partido p) // busca un partido con fecha, hora, nroCancha
-	{	PreparedStatement ps=null;
-		ResultSet rs=null;
+	{	String getOneStatement="SELECT p.fecha,p.hora,p.resultado,p.incidencias, eq1.id,eq1.razonSocial,eq1.localidad,eq1.puntaje,eq1.difGoles, eq2.id,eq2.razonSocial,eq2.localidad,eq2.puntaje,eq2.difGoles, c.numCancha,c.nombre, arb.dniArbitro, arb.nombre, arb.apellido, arb.fechaNac FROM partido p inner join equipo eq1 on p.idEquipo1=eq1.id inner join equipo eq2 on p.idEquipo2=eq2.id inner join cancha c on p.numCancha=c.numCancha left join arbitro arb on arb.dniArbitro=p.dniArbitro where p.fecha=? and p.hora=? and p.numCancha=?;";	
 		Partido partido = new Partido();
-		try {
-			Equipo equipo1=new Equipo();
-			Equipo equipo2=new Equipo();
-			Cancha cancha=new Cancha();
-			ps =DbConnector.getInstancia().getConn().prepareStatement("SELECT p.fecha,p.hora,p.resultado,p.incidencias,"
-							+ "eq1.id,eq1.razonSocial,eq1.localidad,eq1.puntaje,eq1.difGoles,"
-							+ "eq2.id,eq2.razonSocial,eq2.localidad,eq2.puntaje,eq2.difGoles,"
-							+ "c.numCancha,c.nombre"
-							+ "FROM partido p inner join equipo eq1 on p.idEquipo1=eq1.id "
-							+ "inner join equipo eq2 on p.idEquipo2=eq2.id "
-							+ "inner join cancha c on p.numCancha=c.numCancha where p.fecha=? and p.hora=? and p.numCancha=?;"); // hacer join con equipo,chancha y arbitro	
+		try(PreparedStatement ps=DbConnector.getInstancia().getConn().prepareStatement(getOneStatement);) {			
 			ps.setObject(1, p.getFecha());
 			ps.setObject(2, p.getHora());
 			ps.setInt(3, p.getCancha().getNroCancha());
-			rs=ps.executeQuery();  
-			rs.next();		
-			partido.setFecha(rs.getObject("fecha", LocalDate.class));
-			partido.setHora(rs.getObject("hora", LocalTime.class));
-			partido.setResultado(rs.getString("resultado"));
-			partido.setIncidencias(rs.getString("incidencias"));
-			equipo1.setIdEquipo(rs.getInt("eq1.id"));
-			equipo1.setNombre(rs.getString("eq1.razonSocial"));
-			equipo1.setLocalidad(rs.getString("eq1.localidad"));
-			equipo1.setPuntaje(rs.getInt("eq1.puntaje"));
-			equipo1.setDifGoles(rs.getInt("eq1.difGoles"));
-			equipo2.setIdEquipo(rs.getInt("eq2.id"));
-			equipo2.setNombre(rs.getString("eq2.razonSocial"));
-			equipo2.setLocalidad(rs.getString("eq2.localidad"));
-			equipo2.setPuntaje(rs.getInt("eq2.puntaje"));
-			equipo2.setDifGoles(rs.getInt("eq2.difGoles"));
-			cancha.setNroCancha(rs.getInt("c.numCancha"));
-			cancha.setNombre(rs.getString("c.nombre"));
-			partido.setEquipo1(equipo1);
-			partido.setEquipo2(equipo2);
-			partido.setCancha(cancha);				 
+			try(ResultSet rs=ps.executeQuery();){
+				while(rs.next()) {	
+					Equipo equipo1=new Equipo();
+					Equipo equipo2=new Equipo();
+					Cancha cancha=new Cancha();
+					Arbitro arbitro= new Arbitro();					
+					partido.setFecha(rs.getObject("fecha", LocalDate.class));
+					partido.setHora(rs.getObject("hora", LocalTime.class));
+					partido.setResultado(rs.getString("resultado"));
+					partido.setIncidencias(rs.getString("incidencias"));
+					equipo1.setIdEquipo(rs.getInt("eq1.id"));
+					equipo1.setNombre(rs.getString("eq1.razonSocial"));
+					equipo1.setLocalidad(rs.getString("eq1.localidad"));
+					equipo1.setPuntaje(rs.getInt("eq1.puntaje"));
+					equipo1.setDifGoles(rs.getInt("eq1.difGoles"));
+					equipo2.setIdEquipo(rs.getInt("eq2.id"));
+					equipo2.setNombre(rs.getString("eq2.razonSocial"));
+					equipo2.setLocalidad(rs.getString("eq2.localidad"));
+					equipo2.setPuntaje(rs.getInt("eq2.puntaje"));
+					equipo2.setDifGoles(rs.getInt("eq2.difGoles"));
+					cancha.setNroCancha(rs.getInt("c.numCancha"));
+					cancha.setNombre(rs.getString("c.nombre"));
+					arbitro.setDni(rs.getString("arb.dniArbitro"));
+					arbitro.setNombre(rs.getString("arb.nombre"));
+					arbitro.setApellido(rs.getString("arb.apellido"));
+					arbitro.setFecha_nacimiento(rs.getObject("arb.fechaNac",LocalDate.class));
+					partido.setEquipo1(equipo1);
+					partido.setEquipo2(equipo2);
+					partido.setCancha(cancha);		
+					partido.setArbitro(arbitro);
+					}	
+				}
 		    } catch (SQLException ex) {
 		        ex.printStackTrace();
 		    }
 		finally {
 			try {
-				if (ps!= null) {ps.close();}				
-				if (rs != null) {rs.close();}
 				DbConnector.getInstancia().releaseConn();
 			} catch (Exception e2) {
 				e2.printStackTrace();
@@ -117,24 +111,22 @@ public class DataPartido {
 		return partido;
 	}
 	public void update(Partido p) {
-		PreparedStatement ps= null;
-		try { 
-			ps =DbConnector.getInstancia().getConn().prepareStatement("update partido set resultado = ?, idEquipo1 =?, idEquipo2=?, dniArbitro=? where  fecha=? and hora =? and numCancha= ?" );
+		String updateStatement="update partido set resultado = ?, incidencias=?, idEquipo1 =?, idEquipo2=?, dniArbitro=? where  fecha=? and hora =? and numCancha= ?" ;
+		try(PreparedStatement ps= DbConnector.getInstancia().getConn().prepareStatement(updateStatement);) { 
 			ps.setString(1, p.getResultado());
-			ps.setInt(2, p.getEquipo1().getIdEquipo());
-			ps.setInt(3, p.getEquipo2().getIdEquipo());
-			ps.setString(4, p.getArbitro().getDni());
-			//stmt.setString(2, p.getIncidencias());
-			ps.setObject(5, p.getFecha());
-			ps.setObject(6, p.getHora());
-			ps.setInt(7, p.getCancha().getNroCancha());
+			ps.setString(2, p.getIncidencias());
+			ps.setInt(3, p.getEquipo1().getIdEquipo());
+			ps.setInt(4, p.getEquipo2().getIdEquipo());
+			ps.setString(5, p.getArbitro().getDni());
+			ps.setObject(6, p.getFecha());
+			ps.setObject(7, p.getHora());
+			ps.setInt(9, p.getCancha().getNroCancha());
 			ps.executeUpdate();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 		finally {
 			try {
-				if (ps!= null) {ps.close();}	
 				DbConnector.getInstancia().releaseConn();				
 			} catch (Exception e2) {
 				e2.printStackTrace();
@@ -142,14 +134,12 @@ public class DataPartido {
 		}
 	}
 	public void add (Partido p) {	   
-   		PreparedStatement ps=null;
-        try {
-    		ps=DbConnector.getInstancia().getConn().prepareStatement("insert into partido(fecha,hora,idEquipo1,idEquipo2,numCancha,resultado) values (?,?,?,?,?,?)"); // no pongo incidencias ni resultado porque es un partido nuevo
+   		String addStatement="insert into partido(fecha,hora,idEquipo1,idEquipo2,numCancha,resultado) values (?,?,?,?,?,?)";
+        try(PreparedStatement ps=DbConnector.getInstancia().getConn().prepareStatement(addStatement);){  		
     		ps.setObject(1, p.getFecha());
     		ps.setObject(2, p.getHora());
 			ps.setInt(3, p.getEquipo1().getIdEquipo());
 			ps.setInt(4,p.getEquipo2().getIdEquipo());
-			//ps.setInt(5,p.getDniArbitro());
 			ps.setInt(5,p.getCancha().getNroCancha());
 			ps.setString(6, p.getResultado());
             ps.executeUpdate();          
@@ -157,8 +147,7 @@ public class DataPartido {
         	ex.printStackTrace();
         }
         finally {
-			try {
-				if (ps!= null) {ps.close();}		
+			try {		
 				DbConnector.getInstancia().releaseConn();
 			} catch (Exception e2) {
 				e2.printStackTrace();
@@ -166,9 +155,8 @@ public class DataPartido {
 		}
 	}
 	public void delete(Partido p) {
-		PreparedStatement ps=null;
-	    try {	    	
-	    	ps = DbConnector.getInstancia().getConn().prepareStatement("delete from partido where fecha=? and hora=? and numCancha=?");
+		String deleteStatement="delete from partido where fecha=? and hora=? and numCancha=?";	
+	    try(PreparedStatement ps=DbConnector.getInstancia().getConn().prepareStatement(deleteStatement);) {	    	
 			ps.setObject(1, p.getFecha());
 			ps.setObject(2, p.getHora());
 			ps.setInt(3, p.getCancha().getNroCancha());		
@@ -178,7 +166,6 @@ public class DataPartido {
 	    }
 	    finally {
 			try {
-				if (ps!= null) {ps.close();}	
 				DbConnector.getInstancia().releaseConn();
 			} catch (Exception e2) {
 				e2.printStackTrace();
